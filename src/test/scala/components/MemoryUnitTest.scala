b9474ab3424c9633bbc26c9b06ff150e61880684
Jason Lowe-Power <jason@lowepower.com>
2018-11-19 18:58:26 -0800

    Got memory working
    Signed-off-by: Jason Lowe-Power <jason@lowepower.com>

--- src/test/scala/components/MemoryUnitTest.scala
+++ src/test/scala/components/MemoryUnitTest.scala
@@ -2,12 +2,31 @@
 package CODCPU
 
 
+import java.nio.{ByteBuffer, ByteOrder}
+import java.nio.file.{Files, Paths}
+
 import chisel3.iotesters.{ChiselFlatSpec, Driver, PeekPokeTester}
 
 class MemoryUnitTester(c: SimpleAsyncMemory) extends PeekPokeTester(c) {
-  poke(c.io.dataPort.address, 0)
-  poke(c.io.dataPort.memread, 1)
-  expect(c.io.dataPort.readdata, 0x00500333)
+  def load_memory(filename: String, memory: SimpleAsyncMemory) = {
+    val buf = ByteBuffer.wrap(Files.readAllBytes(Paths.get(filename)))
+    buf.order(ByteOrder.LITTLE_ENDIAN) // WHY WOULD THIS DEFAULT TO BIG ENDIAN???
+    val lim = buf.limit()
+    println(s"found $lim bytes")
+    var word = 0
+    while(buf.hasRemaining()) {
+      val data = buf.getInt()
+      println(s"$data")
+      poke(memory.io.dataPort.writedata, data)
+      poke(memory.io.dataPort.address, word << 2) // Doing words, not bytes
+      poke(memory.io.dataPort.memwrite, true)
+      step(1)
+      word += 1
+    }
+  }
+  load_memory("tests/add1.raw", c)
+  poke(c.io.instPort.address, 0)
+  expect(c.io.instPort.instruction, 0x00500333)
 }
 
 
